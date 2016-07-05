f54eb1fa1949f185891eaca7730710a5db03281c
andrewolmos <andrewolmos13@gmail.com>
2016-07-04 23:25:10 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include_once("home.html"); ?>
+<?php include_once("Role Model"); ?>
+<?php include_once("CSS.css"); ?>
+<?php include_once("Dream 1"); ?>
+<?php include_once("Dream 2"); ?>
+<?php include_once("Dream 3"); ?>
